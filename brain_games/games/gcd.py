405b7fe2fb6b1c272ad29b51d28882949ba63996
STEVS <stevs@DESKTOP-2CKJJFK.localdomain>
2022-05-31 22:12:54 +0300

    fiks gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,6 +5,6 @@ from math import gcd
 def start():
     a = randint(1, 100)
     b = randint(1, 100)
-    question = str(a) + ' ' + ' ' + str(b)
+    question = str(a) + ' ' + str(b)
     answer = str(gcd(a, b))
     return question, answer
