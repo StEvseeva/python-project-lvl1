835e9bc172538f55be65117a9a0b85d5458b0cac
STEVS <stevs@DESKTOP-2CKJJFK.localdomain>
2022-06-01 14:12:10 +0300

    fiks progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,12 +9,10 @@ def start():
     empty_place = number + randint(0, count) * step
     progression = ''
     answer = 'place' + str(empty_place)
-    print(number, end, step, empty_place)
-    for number in range(number, end, step):
+    for number in range(number, end + step, step):
         if number != empty_place:
             progression += (str(number) + ' ')
         else:
             progression += '.. '
             answer = str(number)
-    print("ANSWER", answer)
     return progression, answer
